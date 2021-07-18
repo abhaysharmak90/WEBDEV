b4bb6d24077ef1a6fef27b2a2028198f68790771
abhaysharmak90 <abhaysharmak90@gmai.com>
2021-07-18 11:04:49 +0530

    --

--- JS-projects/etch-a-sketch/index.js
+++ JS-projects/etch-a-sketch/index.js
@@ -1,27 +1,26 @@
+const gridSizeBtn10 = document.querySelector("#gridsize-10");
 const gridSizeBtn50 = document.querySelector("#gridsize-50");
-const gridSizeBtn100 = document.querySelector("#gridsize-100");
 const gridContainer = document.getElementById("container");
 const clearBtn = document.getElementById("clear-btn");
-// console.log(clearBtn);
 
 let gridSize;
 let gridDivs = "";
 
-gridSizeBtn50.addEventListener("click", () => {
-  gridSize = 50;
-  showGrid();
+gridSizeBtn10.addEventListener("click", () => {
+  gridSize = 10;
+  showDivs();
 });
 
-gridSizeBtn100.addEventListener("click", () => {
-  gridSize = 100;
-  showGrid();
+gridSizeBtn50.addEventListener("click", () => {
+  gridSize = 50;
+  showDivs();
 });
 
 clearBtn.addEventListener("dblclick", clearGrid);
 
 function createGridDivs(noOfDivs) {
   for (let i = 1; i <= noOfDivs * noOfDivs; i++) {
-    gridDivs += `<div class="griditems">${i}</div>`;
+    gridDivs += `<div class="griditems"></div>`;
   }
 }
 
@@ -30,7 +29,7 @@ function clearGrid() {
   gridContainer.innerHTML = gridDivs;
 }
 
-function showGrid() {
+function showDivs() {
   clearGrid();
   createGridDivs(gridSize);
   gridContainer.innerHTML = gridDivs;
